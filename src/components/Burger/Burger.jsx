11728f36473e56b9eb7a402c8d21f6fbcca57f9f
ionescuflorin <florineugen96@gmail.com>
2019-11-05 11:17:23 +0200

    Added case when we don't have ingredients

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -5,13 +5,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const Burger = props => {
   // transform the ingredients object in an array so we can use map function
   // Object.keys - extracts the keys of a given obj and turns that in an array (the values 1122 are not part of the array)
-
   // see ref1
-  const transformedingredients = Object.keys(props.ingredients).map(igKey => {
+  let transformedingredients = Object.keys(props.ingredients).map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
       return <BurgerIngredient key={igKey + i} type={igKey} />;
     });
-  });
+  }) // here if we have 0 for all ingredients it will be returned different empty arrays so we can't check if it's empty or not
+.reduce((arr, el) => {
+    return arr.concat(el) // now we have an array that can be either empty or having ingredients
+}, []);
+
+if (transformedingredients.length === 0) {
+    transformedingredients = <p>Please start adding ingredients</p>
+}
 
   return (
     <div className="Burger">
