0b5be9fa30c3ddff0ca4db45cf2732c7812c5fb1
ionescuflorin <florineugen96@gmail.com>
2019-11-05 09:41:09 +0200

    Added main components as placeholders

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Layout.css'
+
+
+const Layout = (props) => {
+    return (
+        <React.Fragment>
+            <div>Toolbar, SideDrawer, Backdrop</div>
+            <main className='Content'>
+                {props.children}
+            </main>
+        </React.Fragment>
+    )
+}
+
+export default Layout
---
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BurgerBuilder extends React.Component {
+    render() {
+        return (
+           <React.Fragment>
+               <div>Burger</div>
+               <div>Build Control</div>
+           </React.Fragment>
+        );
+    }
+}
+
+export default BurgerBuilder;
