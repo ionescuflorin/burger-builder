2ad18ecafe4f88aed26df7495e6f7efc36e511dc
ionescuflorin <florineugen96@gmail.com>
2019-11-05 16:24:17 +0200

    Disaply Price

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import './BuildControls.css'
-import BuildControl from './BuildControl/BuildControl'
+import React from 'react';
+import './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type:'salad'},
-    {label: 'Bacon', type:'bacon'},
-    {label: 'Cheese', type:'cheese'},
-    {label: 'Meat', type:'meat'}
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
 ];
 
-const BuildControls = (props) => {
-    return (
-        <div className='BuildControls'>
-            {controls.map(ctrl => (
-                <BuildControl added={() => props.ingredientAdded(ctrl.type)} removed={() => props.ingredientRemoved(ctrl.type)} disabled={props.disabled[ctrl.type]} key={ctrl.label} label={ctrl.label}/>
-            ))}
-        </div>
-    )
-}
+const BuildControls = props => {
+  return (
+    <div className="BuildControls">
+        <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
+      {controls.map(ctrl => (
+        <BuildControl
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+          key={ctrl.label}
+          label={ctrl.label}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default BuildControls
+export default BuildControls;
