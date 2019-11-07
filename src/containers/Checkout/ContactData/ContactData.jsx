b37ae4a8e318564df8e27787d7f33138f9f73b4d
ionescuflorin <florineugen96@gmail.com>
2019-11-07 11:27:30 +0200

    [FORM] deliveryMethod field fixed minor bug

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -83,7 +83,7 @@ class ContactData extends React.Component {
             { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
-        value: '',
+        value: 'cheapest',
         validation: {},
         valid: true
       
