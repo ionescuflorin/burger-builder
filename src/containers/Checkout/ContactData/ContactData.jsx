0563ba10600e0bde12940cd7366b7173057bf68d
ionescuflorin <florineugen96@gmail.com>
2019-11-07 10:33:43 +0200

    [FORM] Adding validation

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -14,7 +14,11 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false
       },
       street: {
         elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false
       },
       zipCode: {
         elementType: 'input',
@@ -30,7 +38,13 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Zip Code'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
       country: {
         elementType: 'input',
@@ -38,7 +52,11 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false
       },
       email: {
         elementType: 'input',
@@ -46,7 +64,11 @@ class ContactData extends React.Component {
           type: 'email',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -57,6 +79,7 @@ class ContactData extends React.Component {
           ]
         },
         value: ''
+      
       }
     },
     loading: false
@@ -84,6 +107,21 @@ class ContactData extends React.Component {
       .catch(error => this.setState({ loading: false }));
   };
 
+  checkValidity = (value, rules) => {
+    let isValid = true
+    if(rules.required) {
+      // remove whitespaces at the begginig or the end
+      isValid = value.trim() !== '' && isValid;
+    }
+    if(rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+    if(rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+    return isValid
+  }
+
   // Handle user input in a immutable way
   inputChangedHandler = (e, inputIdentifier) => {
     const updatedOrderForm = {
@@ -92,6 +130,7 @@ class ContactData extends React.Component {
     // clone the object state deeply
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
     updatedFormElement.value = e.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     this.setState({orderForm: updatedOrderForm})
   };
